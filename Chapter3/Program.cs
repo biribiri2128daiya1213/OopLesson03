0e7ade75a2832cef058db63e1996b12e5112f7ce
大夜 吉川 <31157@ojs.ac.jp>
2020-09-28 13:46:26 +0900

    問題3-2

--- Chapter3/Program.cs
+++ Chapter3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -9,15 +10,37 @@ using System.Threading.Tasks;
 namespace Chapter3 {
 	class Program {
 		static void Main(string[] args) {
-			var numbers = new List<int> {
-				12,87,94,14,53,20,40,35,76,91,31,17,48,
+			var names = new List<string> {
+				"Tokyo","New Delhi","Bangkok","London","Paris","Berlin","Canberra","Hong Kong",
 			};
 
-			List<int> lists = numbers.Select(n => n * 2).ToList();
+			#region 3-2-1
+			//var line = Console.ReadLine();
 
-			foreach (var n in lists) {
-				Console.WriteLine(n);
+			//var index = names.FindIndex(s => s == line);
+			//Console.WriteLine($"{index}番目");
+			#endregion
+
+			#region 3-2-2
+			//var count = names.Count(s => s.Contains('o'));
+			//Console.WriteLine(count);
+			#endregion
+
+			#region 3-2-3			
+			//var list = names.Where(s => s.Contains('o'));
+			//string[] array = list.ToArray();
+
+			//foreach (var n in array) {
+			//	Console.WriteLine(n);
+			//}
+			#endregion
+
+			#region 3-2-4
+			var length = names.Where(s => s[0] == 'B').Select(s => s.Length);
+			foreach (var len in length) {
+				Console.WriteLine(len);
 			}
+			#endregion
 		}
 	}
 }
