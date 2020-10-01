20ec2935d3b42ae05c892e6b20612d12c6d8a2c7
大夜 吉川 <31157@ojs.ac.jp>
2020-10-01 10:21:19 +0900

    P116 リスト4.27

--- chapter04/Program.cs
+++ chapter04/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,18 @@ using System.Threading.Tasks;
 namespace chapter04 {
 	class Program {
 		static void Main(string[] args) {
+			string code = "12345";
+
+			var message = GetMessage(code) ?? DefaultMessage();
+			Console.WriteLine(message);
+		}
+		//スタブ
+		private static object DefaultMessage() {
+			return "DefaultMessage";
+		}
+		//スタブ
+		private static object GetMessage(string code) {
+			return code;
 		}
 	}
 }
